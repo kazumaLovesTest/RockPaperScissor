03f72d2cd4f0f7ebda3ef8cff734bcb98ad24a13
kazumaneet <mekbibtare@gmail.com>
2021-10-26 12:09:32 +0300

    implemented the play round function

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 function computerPlay(){
-   const choice = Math.floor(Math.random() * (4 - 1) + 1);
-   console.log(choice);
+   let choice = Math.floor(Math.random() * (4 - 1) + 1);
    if (choice === 1)
     return "rock";
    else if(choice === 2)
@@ -8,3 +7,18 @@ function computerPlay(){
    else
     return "scissor"; 
 }
+function playRound(computerSelection,playerSelection){
+    
+    console.log(computerSelection);
+    if (computerSelection === "rock" && playerSelection === "rock"){
+        console.log("tie");
+    }
+    else if (computerSelection == "paper" && playerSelection === "rock"){
+        console.log("You lose. paper beats rock");
+    }
+    else
+        console.log("You win. rock beats scissor");;
+}
+const playerSelection = "rock";
+let computerSelection = computerPlay();
+playRound(computerSelection,playerSelection);
