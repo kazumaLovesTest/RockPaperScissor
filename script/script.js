6bdab3406aeaeac49773dd45edcb5316a45c60b8
kazumaneet <mekbibtare@gmail.com>
2021-10-26 11:27:13 +0300

    implemented the computerPlay function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+function computerPlay(){
+   const choice = Math.floor(Math.random() * (4 - 1) + 1);
+   console.log(choice);
+   if (choice === 1)
+    return "rock";
+   else if(choice === 2)
+    return "paper";
+   else
+    return "scissor"; 
+}
