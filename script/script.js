008ef96ba91f3dcf77a90c754ef517638cc12ce1
kazumaneet <mekbibtare@gmail.com>
2021-11-05 17:13:29 +0300

    added a play again implementation

--- script/script.js
+++ script/script.js
@@ -3,12 +3,25 @@ const buttons = document.querySelectorAll('button');
 const score = document.querySelector('#score');
 let playerScore = 0;
 let computerScore = 0;
+play();
+function play(){
+    playerScore = 0;
+    computerScore = 0;
+    buttons.forEach((button) => {
+        button.addEventListener('click', _listener);
+    });
+    results.textContent = '';
+    score.textContent = '';
+    winMessage.textContent ='';
+    
+}
 function _listener(e){
     selectChoice(e.target.id);
 }
-buttons.forEach((button) => {
-    button.addEventListener('click', _listener);
-});
+
+// buttons.forEach((button) => {
+//     button.addEventListener('click', _listener);
+// });
 
 function selectChoice(buttonId){
     const playerSelection = buttonId.toLowerCase();
@@ -79,11 +92,20 @@ function gameEndCheck(playerScore,computerScore){
             button.removeEventListener('click',_listener);
         })
     const winMessage = document.querySelector('#winMessage');
+    const playAgain = document.createElement('button');
+    
+    
     if (playerScore === 5)
         winMessage.textContent = "congratualations you won the game";
     else
         winMessage.textContent = "Better luck next time buddy";
-    }
 
+    playAgain.textContent = "playagain";
+    playAgain.addEventListener('click', play);
+    winMessage.append(playAgain);
+    
+    }
+    
 }
+
 
